94b2ea666af20e600d0203ac14c34228c0cc46eb
lucas-linux <almeidalucas820@gmail.com>
2023-08-08 18:09:05 -0300

    creating project, Assets and Splash, Adding the Styled Components,

---
+++ src/screens/SignIn/index.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import { styles } from "./styles";
+
+export function SignIn() {
+
+    return (
+        <View style={styles.container}>
+            <Text>Open up App.tsx to start working on your app!</Text>
+        </View>
+    )
+}
