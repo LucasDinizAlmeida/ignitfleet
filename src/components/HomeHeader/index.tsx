f4755bb243427d532ebe59d38571d8ddd938dbd8
lucas-linux <almeidalucas820@gmail.com>
2023-08-11 17:39:32 -0300

    installing phosphor-react-native and react-native-svg libraries

--- src/components/HomeHeader/index.tsx
+++ src/components/HomeHeader/index.tsx
@@ -1,4 +1,7 @@
+import { TouchableOpacity } from 'react-native';
 import { Container, Greeting, Message, Name } from './styles';
+import { Power } from 'phosphor-react-native'
+import theme from '../../theme';
 
 export function HomeHeader() {
   return (
@@ -11,6 +14,10 @@ export function HomeHeader() {
           Lucas
         </Name>
       </Greeting>
+
+      <TouchableOpacity>
+        <Power size={32} color={theme.COLORS.GRAY_400}/>
+      </TouchableOpacity>
     </Container>
   );
 }
