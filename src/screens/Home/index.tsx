96aec05278e1477715b25dc863c683db17b30ef5
lucas-linux <almeidalucas820@gmail.com>
2023-08-13 22:52:41 -0300

    Banco de Dados local - Observando mudanças no banco

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,15 +3,18 @@ import { Container, Content } from './styles';
 import { HomeHeader } from '../../components/HomeHeader';
 import { CarStatus } from '../../components/CarStatus';
 import { useNavigation } from '@react-navigation/native';
-import { useQuery } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
+
+import { useQuery, useRealm } from '../../libs/realm';
 
 export function Home() {
 
   const [vehicleInUse, setVehicleInUse] = useState<Historic | null>(null)
 
   const { navigate } = useNavigation()
+
   const historic = useQuery(Historic)
+  const realm = useRealm()
 
   function handleRegisterMovement() {
     if(vehicleInUse?._id) {
@@ -21,13 +24,15 @@ export function Home() {
     }
   }
 
-  function fetchVehicle() {
+  function fetchVehicleInUse() {
     const vehicle = historic.filtered("status = 'departure'")[0]
     setVehicleInUse(vehicle)
   }
 
   useEffect(() => {
-    fetchVehicle()
+    realm.addListener('change', () => fetchVehicleInUse())
+
+    return () => realm.removeListener('change', fetchVehicleInUse)
   }, [])
 
   return (
