afd01cd91d3611965b6dc50218ada0e09c641a9c
lucas-linux <almeidalucas820@gmail.com>
2023-08-18 00:31:07 -0300

    Geolocalização - Obtendo as coordenadas da localização atual

--- src/screens/Departure/index.tsx
+++ src/screens/Departure/index.tsx
@@ -8,7 +8,7 @@ import { TextAreaInput } from '../../components/TextAreaInput';
 import { Button } from '../../components/Button';
 import { licensePlateValidate } from '../../utils/licensesPlateValidate';
 
-import { useForegroundPermissions } from 'expo-location';
+import { useForegroundPermissions, watchPositionAsync, LocationAccuracy, LocationSubscription } from 'expo-location';
 
 import { useRealm } from '../../libs/realm';
 import { Historic } from '../../libs/realm/schemas/Historic';
@@ -67,6 +67,24 @@ export function Departure() {
   useEffect(() => {
     requestLocationForegroundPermission()
   }, [])
+
+  useEffect(() => {
+    if(!locationForegroundPermission?.granted) {
+      return
+    }
+
+    let subscription: LocationSubscription
+
+    watchPositionAsync({
+      accuracy: LocationAccuracy.High,
+      timeInterval: 1000
+    }, (location) => {
+      console.log(location)
+    })
+      .then(response => subscription = response)
+
+    return () => subscription.remove()
+  }, [locationForegroundPermission])
 
   if(!locationForegroundPermission?.granted) {
      return (
